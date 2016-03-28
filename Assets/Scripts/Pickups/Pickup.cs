6ba36daaa08461e8a83ac614c71f9dbe056d0dcf
chaitanyakochhar <seeck.sleek@gmail.com>
2016-03-28 17:58:59 -0400

    Added shaking aliens, alien pickup, and UI.

---
+++ Assets/Scripts/Pickups/Pickup.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Pickup : MonoBehaviour
+{
+    private static int counter = 1;
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            GameObject alienUI = GameObject.Find("Alien " + counter);
+            Color c = alienUI.GetComponent<Image>().color;
+            c.a = 255;
+            alienUI.GetComponent<Image>().color = c;
+            counter++;
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Pickups/ShakeLerp.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShakeLerp : MonoBehaviour {
+
+    public float OscillationCount = 3f;
+    public float intervalBetweenOscillations = 1f;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(Oscillate());
+	}
+	
+    private IEnumerator Oscillate()
+    {
+        while (true)
+        {
+            float startTime = Time.time;
+            float pingPong; 
+            while ((Time.time - startTime)<= OscillationCount)
+            {
+                pingPong = -1 + Mathf.PingPong(Time.time, 2f);
+                transform.Rotate(Vector3.back, 1f * pingPong);
+                yield return null;
+            }
+            yield return null;
+        }
+    }
+}
