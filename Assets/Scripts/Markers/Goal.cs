9e2a6725787c109912e0ef3a8c6443959bf8eabc
chaitanyakochhar <seeck.sleek@gmail.com>
2016-03-29 10:48:19 -0400

    Added in goal prefab

---
+++ Assets/Scripts/Markers/Goal.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour
+{
+    public string nextLevel = "DodgingAsteroids_Dummy";
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            SceneManager.LoadScene(nextLevel);
+        }
+    }
+}
