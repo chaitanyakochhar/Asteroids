3bc6744db2b620b47e84c37952f6f9a55fee8285
chaitanyakochhar <seeck.sleek@gmail.com>
2016-04-22 15:50:35 -0400

    Added PlayOnClick

---
+++ Assets/_Templates/AudioPlayback/StartPlayOnClick.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[RequireComponent(typeof(AudioSource))]
+public class StartPlayOnClick : Effect
+{
+    public float delay;
+    public AudioClip clip;
+
+    public override IEnumerator EffectCoroutine()
+    {
+        yield return new WaitForSeconds(delay);
+        if(clip!=null)
+        {
+            GetComponent<AudioSource>().PlayOneShot(clip);
+        }
+    }
+
+    public override void StartEffect()
+    {
+        StartCoroutine(EffectCoroutine());
+    }
+}
