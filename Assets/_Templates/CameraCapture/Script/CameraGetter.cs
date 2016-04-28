4556ed4ee91d65edc7758f3e2889bd993e147460
chaitanyakochhar <seeck.sleek@gmail.com>
2016-04-28 11:40:24 -0400

    Added rotate logic for iOS

--- Assets/_Templates/CameraCapture/Script/CameraGetter.cs
+++ Assets/_Templates/CameraCapture/Script/CameraGetter.cs
@@ -9,6 +9,11 @@ public class CameraGetter : MonoBehaviour
     {
         Texture2D t = null;
         bool test = false;
+        if(Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            transform.Rotate(new Vector3(0, 0, 180));
+        }
+
         if (GameObject.Find("Data Manager") != null)
             test = GameObject.Find("Data Manager").GetComponent<PlayerDataManager>().imagesCaptured.TryGetValue(key, out t);
         if (test)
