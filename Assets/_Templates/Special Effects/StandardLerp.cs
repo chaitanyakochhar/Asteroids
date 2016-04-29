6dc94fd36a575752d9aa70bddf5f4a74fa3f9f2b
chaitanyakochhar <seeck.sleek@gmail.com>
2016-04-29 12:32:43 -0400

    Created the UI Jump script. Aliens jump now. Fuh reel..

---
+++ Assets/_Templates/Special Effects/StandardLerp.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class StandardLerp : Effect
+{
+    public GameObject destination;
+    public float duration;
+    public bool PLAY_ON_START = false;
+
+    public void Start()
+    {
+        if(PLAY_ON_START)
+        {
+            StartCoroutine(EffectCoroutine());
+        }
+    }
+
+    public override IEnumerator EffectCoroutine()
+    {
+        float startTime = Time.time;
+        Vector3 startPosition = transform.position;
+        while((Time.time - startTime)<=duration)
+        {
+            transform.position = Vector3.Lerp(startPosition, destination.transform.position, (Time.time - startTime) / duration);
+            yield return null;
+        }
+    }
+
+    public override void StartEffect()
+    {
+        StartCoroutine(EffectCoroutine());
+    }
+}
---
+++ Assets/_Templates/Special Effects/UIJump.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class UIJump : Effect
+{
+    public Vector3 displacement;
+    public AudioClip clip;
+    public float duration = 1f;
+    public bool PLAY_EFFECT_ON_START = false;
+    public void Start()
+    {
+        if(PLAY_EFFECT_ON_START)
+        {
+            StartEffect();
+        }
+    }
+
+    public override void StartEffect()
+    {
+        StartCoroutine(EffectCoroutine());
+    }
+
+    public override IEnumerator EffectCoroutine()
+    {
+        float startTime = Time.time;
+        Vector3 startPos = GetComponent<RectTransform>().anchoredPosition3D;
+        Vector3 endPos = GetComponent<RectTransform>().anchoredPosition3D + displacement;
+        while (true)
+        {
+            if (clip != null)
+            {
+                Camera.main.GetComponent<AudioSource>().PlayOneShot(clip);
+            }
+            startTime = Time.time;
+            while ((Time.time - startTime) <= duration)
+            {
+                GetComponent<RectTransform>().anchoredPosition3D = Vector3.Lerp(startPos, endPos, (Time.time - startTime) / duration);
+                yield return null;
+            }
+            startTime = Time.time;
+            while ((Time.time - startTime) <= duration)
+            {
+                GetComponent<RectTransform>().anchoredPosition3D = Vector3.Lerp(endPos, startPos, (Time.time - startTime) / duration);
+                yield return null;
+            }
+            yield return null;
+        }
+    }
+}
