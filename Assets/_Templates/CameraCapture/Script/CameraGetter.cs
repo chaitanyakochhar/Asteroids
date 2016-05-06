f832055f7860c9f5d4e041d67fa2c62c4505f2e0
Chaitanya Kochhar <Chaitanya Kochhar>
2016-05-06 14:10:40 -0400

    Added in the solar system

--- Assets/_Templates/CameraCapture/Script/CameraGetter.cs
+++ Assets/_Templates/CameraCapture/Script/CameraGetter.cs
@@ -1,19 +1,27 @@
 ﻿using UnityEngine;
 using System;
 using UnityEngine.UI;
+using System.Collections;
 
-public class CameraGetter : MonoBehaviour
+public class CameraGetter : Effect
 {
     public string key;
     public bool isWebcamPicture = true;
     public void Start()
     {
-        Texture2D t = null;
-        bool test = false;
+
         if(Application.platform == RuntimePlatform.IPhonePlayer && isWebcamPicture)
         {
             transform.Rotate(new Vector3(0, 0, 180));
         }
+
+        StartEffect();
+    }
+
+    public override void StartEffect()
+    {
+        Texture2D t = null;
+        bool test = false;
 
         if (GameObject.Find("Data Manager") != null)
             test = GameObject.Find("Data Manager").GetComponent<PlayerDataManager>().imagesCaptured.TryGetValue(key, out t);
@@ -25,6 +33,16 @@ public class CameraGetter : MonoBehaviour
                 GetComponent<RawImage>().texture = t;
             t.Apply();
         }
+    }
+
+    public override void StopEffect()
+    {
+        ;
+    }
+
+    public override IEnumerator EffectCoroutine()
+    {
+        yield return null;
     }
 }
 
---
+++ Assets/_Templates/Special Effects/ZoomOut2D.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+[RequireComponent(typeof(Authenticator))]
+public class ZoomOut2D : Effect
+{
+    public bool START_EFFECT_ON_START = false;
+    public float initialDelay = 0;
+    public float duration;
+    public float viewPortStart;
+    public float viewPortEnd;
+    public GameObject lerpDestination;
+
+    private float startTime;
+    private Camera camera;
+    private Vector3 startPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        camera = Camera.main;
+        startPosition = camera.transform.position;
+        if(START_EFFECT_ON_START)
+        {
+            StartEffect();
+        }
+    }
+
+    public override IEnumerator EffectCoroutine()
+    {
+        yield return new WaitForSeconds(initialDelay);
+        startTime = Time.time;
+        while((Time.time - startTime)<=duration)
+        {
+            camera.orthographicSize = Mathf.Lerp(viewPortStart, viewPortEnd, (Time.time - startTime) / duration);
+            camera.transform.position = Vector3.Lerp(startPosition, lerpDestination.transform.position, (Time.time - startTime) / duration);
+            yield return null;
+        }
+        GetComponent<Authenticator>().isAuthenticated = true;
+
+    }
+
+    public override void StartEffect()
+    {
+        StartCoroutine(EffectCoroutine());
+    }
+
+    public override void StopEffect()
+    {
+        StopAllCoroutines();
+    }
+}
