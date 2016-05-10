cddd1fce775b5e91d0ad4d4693598bcdd125a21c
chaitanyakochhar <seeck.sleek@gmail.com>
2016-05-10 18:45:37 -0400

    Wrote some scripts

---
+++ Assets/_Templates/Cross Platform Input/CrossPlatformInput.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrossPlatformInput : MonoBehaviour
+{
+
+    public bool IgnoreUI = true;
+
+    void Update()
+    {
+        MouseListener();
+        TouchListener();
+    }
+    
+    private void MouseListener()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Command c = Command.createCommandWithHitObjectReferenceIgnoreUI(Input.mousePosition, out hit);
+            if(c!=null)
+            {
+                if(hit.transform.GetComponent<AnimateOnClick>()!= null)
+                {
+                    hit.transform.GetComponent<AnimateOnClick>().StartEffect();
+                }
+            }
+        }
+    }
+
+    private void TouchListener()
+    {
+        if(Input.touchCount>0)
+        {
+            Touch t = Input.touches[0];
+            switch(t.phase)
+            {
+                case TouchPhase.Began:
+                    {
+                        RaycastHit hit;
+                        Command c = Command.createCommandWithHitObjectReferenceIgnoreUI(t.position, out hit);
+                        break;
+                    }
+            }
+        }
+    }
+}
---
+++ Assets/_Templates/Special Effects/AnimateOnClick.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[RequireComponent(typeof(Animation))]
+public class AnimateOnClick : Effect
+{
+    public bool START_ON_PLAY = false;
+
+    public override IEnumerator EffectCoroutine()
+    {
+        GetComponent<Animation>().Play();
+        yield return new WaitForSeconds(2);
+        GetComponent<Animation>().Stop();
+    }
+
+    public override void StartEffect()
+    {
+        StartCoroutine(EffectCoroutine());
+    }
+
+    public override void StopEffect()
+    {
+        StopAllCoroutines();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        if (START_ON_PLAY)
+        {
+            StartEffect();
+        }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        print("Boom!");
+    }
+}
