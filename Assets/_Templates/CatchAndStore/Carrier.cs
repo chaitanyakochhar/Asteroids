7193c27a598cee3147449f2cb9175f3fc80cb277
chaitanyakochhar <seeck.sleek@gmail.com>
2016-05-13 16:21:42 -0400

    Created the new game where you give aliens gifts

---
+++ Assets/_Templates/CatchAndStore/Carrier.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Carrier : MonoBehaviour
+{
+
+    public float oneWayDuration = 1f;
+    public bool isGiving { get; set; }
+    public bool Going { get; set; }
+    public bool Done { get; set; }
+
+    public GameObject givingThis { get; set; }
+    public Vector3 startPoint { get; set; }
+    private Vector3 destination;
+    private GameObject instancedObject;
+
+    private void GiveObject(GameObject receiver)
+    {
+        instancedObject.transform.SetParent(receiver.transform);
+    }
+
+    private void CollectObject(GameObject takingThis)
+    {
+        takingThis.transform.SetParent(transform);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(isGiving)
+        {
+            GiveObject(other.gameObject);
+        }
+        else
+        {
+            CollectObject(other.gameObject);
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag != "Player")
+        {
+            if (isGiving)
+            {
+                Done = true;
+                GiveObject(collision.gameObject);
+            }
+            else
+            {
+                Done = true;
+                CollectObject(collision.gameObject);
+            }
+        }
+    }
+
+    public void GoToAndReturn(Vector3 destination)
+    {
+        if(isGiving)
+        {
+            instancedObject = Instantiate(givingThis, transform.position, Quaternion.identity) as GameObject;
+            instancedObject.transform.parent = transform;
+        }
+        Done = false;
+        Going = true;
+        StartCoroutine(GoAndReturnCR(destination, startPoint));
+    }
+
+    private IEnumerator GoAndReturnCR(Vector3 to, Vector3 from)
+    {
+        float startTime = Time.time;
+        float elapsed = Time.time - startTime;
+        while(elapsed<=oneWayDuration)
+        {
+            transform.position = Vector3.Lerp(from, to, elapsed / oneWayDuration);
+            elapsed = Time.time - startTime;
+            yield return null;
+        }
+        startTime = Time.time;
+        elapsed = Time.time - startTime;
+        Going = false;
+        while (elapsed <= oneWayDuration)
+        {
+            transform.position = Vector3.Lerp(to, from, elapsed / oneWayDuration);
+            elapsed = Time.time - startTime;
+            yield return null;
+        }
+        
+    } 
+}
---
+++ Assets/_Templates/CatchAndStore/CatchAndStore.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Authenticator))]
+public class CatchAndStore : MonoBehaviour
+{
+    public bool GivingThemSomething = true;
+    public GameObject playerCharacter;
+    public GameObject givingThis;
+    public GameObject spawnPoint;
+
+    public int numberOfPlayers = 2;
+    public int itemsToGive = 3;
+    public int itemsCollected = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        MouseListener();
+
+    }
+
+    private void MouseListener()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Command c = Command.createCommandWithoutRaycast(Input.mousePosition);
+            if (c != null)
+            {
+                CharacterPingPong(c);
+            }
+
+        }
+    }
+
+    private void CharacterPingPong(Command c)
+    {
+        if (numberOfPlayers > 0)
+        {
+            numberOfPlayers--;
+            GameObject GO = Instantiate(playerCharacter, transform.position, Quaternion.identity) as GameObject;
+            Carrier carrier;
+            if (GO.GetComponent<Carrier>() == null)
+            {
+                carrier = GO.AddComponent<Carrier>();
+            }
+            else
+            {
+                carrier = GO.GetComponent<Carrier>();
+            }
+            carrier.startPoint = transform.position;
+            Vector3 v = c.worldPoint;
+            v.z = transform.position.z;
+            carrier.givingThis = givingThis;
+            carrier.isGiving = GivingThemSomething;
+            carrier.GoToAndReturn(v);
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        Carrier carrier = collision.transform.GetComponent<Carrier>();
+        if (carrier != null && !carrier.Going)
+        {
+            if(carrier.Done)
+            {
+                itemsCollected++;
+                Authenticate();
+            }
+            Destroy(collision.gameObject);
+            numberOfPlayers++;
+        }
+    }
+
+    public void OnTriggerEnter(Collider collision)
+    {
+        Carrier carrier = collision.transform.GetComponent<Carrier>();
+        if (carrier != null && !carrier.Going)
+        {
+            if (carrier.Done)
+            {
+                itemsCollected++;
+                Authenticate();
+            }
+            Destroy(collision.gameObject);
+            numberOfPlayers++;
+        }
+    }
+
+    private void Authenticate()
+    {
+        if (itemsToGive == itemsCollected)
+        {
+            GetComponent<Authenticator>().isAuthenticated = true;
+        }
+    }
+}
