f44b1b500b14359410bfadc43ab45951c5cb7394
Chaitanya Kochhar <Chaitanya Kochhar>
2016-05-18 13:19:42 -0400

    Sound toggle for home page

---
+++ Assets/_Templates/Special Effects/UITogglePlaySound.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class UITogglePlaySound : MonoBehaviour {
+    public AudioClip onSound;
+    public AudioClip offSound;
+    public void ToggleListener(bool value)
+    {
+        if(value)
+        {
+            GetComponent<AudioSource>().PlayOneShot(onSound);
+        }
+        else
+        {
+            GetComponent<AudioSource>().PlayOneShot(offSound);
+        }
+    }
+
+
+}
