46f71d9bcfa83fc08146175ea8c3ce4d83be4ae6
Chaitanya Kochhar <Chaitanya Kochhar>
2016-05-19 15:46:39 -0400

    Reformat

--- Assets/_Templates/Special Effects/UITogglePlaySound.cs
+++ Assets/_Templates/Special Effects/UITogglePlaySound.cs
@@ -1,13 +1,14 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 [RequireComponent(typeof(AudioSource))]
-public class UITogglePlaySound : MonoBehaviour {
+public class UITogglePlaySound : MonoBehaviour
+{
     public AudioClip onSound;
     public AudioClip offSound;
     public void ToggleListener(bool value)
     {
         Toggle t = GetComponent<Toggle>();
-        if(t.isOn)
+        if (t.isOn)
         {
             GetComponent<AudioSource>().PlayOneShot(onSound);
         }
