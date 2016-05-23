8c7395485c3e6e211b1a98da2619fb41e1135bba
Chaitanya Kochhar <Chaitanya Kochhar>
2016-05-23 14:14:35 -0400

    Added the arrows for scrolling back and forth between the text

--- Assets/_Templates/Special Effects/TypeyText2.cs
+++ Assets/_Templates/Special Effects/TypeyText2.cs
@@ -12,11 +12,31 @@ public class TypeyText2 : MonoBehaviour
     public Sprite[] images;
     public float[] delays;
     public float textDelay = 0.1f;
+    public GameObject TextUpButton;
+    public GameObject TextDownButton;
     private Text body;
+
+    private int minText;
+    private int maxText;
+    private int currentText;
+    private Button UpButton;
+    private Button DownButton;
 
 
     public void Start()
     {
+        minText = 0;
+        maxText = textToTypeOut.Length - 1;
+        currentText = maxText;
+
+        if(TextUpButton!= null && TextDownButton!= null)
+        {
+            UpButton = TextUpButton.GetComponent<Button>();
+            DownButton = TextDownButton.GetComponent<Button>();
+            UpButton.interactable = false;
+            DownButton.interactable = false;
+        }
+
         body = GetComponent<Text>();
         if(textToTypeOut.Length>0)
         {
@@ -36,10 +56,9 @@ public class TypeyText2 : MonoBehaviour
         for (int i = 0; i<textToTypeOut.Length;i++)
         {
             body.text = "";
-            
+
             splitText = textToTypeOut[i].Split(separators);
-            imagePlaceholder.GetComponent<Image>().sprite = images[i];
-            body.text = textToTypeOut[i];
+            UpdateTextBox(images[i],textToTypeOut[i]);
             yield return new WaitForSeconds(textDelay * splitText.Length);
             //foreach (string x in splitText)
             //{
@@ -50,8 +69,49 @@ public class TypeyText2 : MonoBehaviour
             yield return new WaitForSeconds(delays[i]);
         }
         GetComponent<Authenticator>().isAuthenticated = true;
+        UpButton.interactable = true;
+        DownButton.interactable = false;
         yield return null;
     }
 
+    private void UpdateTextBox(Sprite image, string text)
+    {
+        imagePlaceholder.GetComponent<Image>().sprite = image;
+        body.text = text;
+    }
 
+    public void ScrollUp()
+    {
+        if (UpButton != null)
+        {
+            currentText--;
+            Validate();
+            UpdateTextBox(images[currentText], textToTypeOut[currentText]);
+        }
+    }
+    public void ScrollDown()
+    {
+        if (DownButton != null)
+        {
+            currentText++;
+            Validate();
+            UpdateTextBox(images[currentText], textToTypeOut[currentText]);
+            
+        }
+    }
+
+    private void Validate()
+    {
+        DownButton.interactable = true;
+        UpButton.interactable = true;
+        if(currentText == maxText)
+        {
+            DownButton.interactable = false;
+        }
+        if(currentText == 0)
+        {
+            UpButton.interactable = false;
+        }
+        
+    }
 }
